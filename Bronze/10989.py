06a75896f62ddb246dde0993b7103886bf6669a0
AI-WonYJ <yjason7587@naver.com>
2023-02-09 21:34:53 +0900

    Bronze 1 10989번: 수 정렬하기 3

---
+++ Bronze/10989.py
@@ -0,0 +1,14 @@
+import sys
+c = int(sys.stdin.readline())
+N_list = [0] * 10000
+
+for i in range(c):
+  N = int(sys.stdin.readline())
+  N_list[N] += 1
+
+for j in range(0, len(N_list)):
+  if N_list[j] == 0:
+    continue
+  else:
+    for k in range(0, N_list[j]):
+      print(j)
