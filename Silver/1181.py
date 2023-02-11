43ab407cb7b0495a7b8380f2a477ba981ce174b1
AI-WonYJ <yjason7587@naver.com>
2023-02-11 16:00:40 +0900

    Silver 5 1181번: 단어 정렬

--- Silver/1181.py
+++ Silver/1181.py
@@ -2,6 +2,17 @@ import sys
 
 N = int(sys.stdin.readline())
 
-for i in range(0, N):
-  Word = sys. stdin.readline()
-  
+Word_list = []
+
+for i in range(N):
+  Word = str(sys.stdin.readline().strip())
+  if Word in Word_list:
+    continue
+  else:
+    Word_list.append(Word)
+    
+Word_list.sort()
+Word_list.sort(key = len)
+
+for j in Word_list:
+    print(j)
