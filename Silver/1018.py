26ac1832076042f08d17dd92116473ada8723679
AI-WonYJ <yjason7587@naver.com>
2023-02-14 17:57:14 +0900

    Silver 4 1018번: 체스판 다시 칠하기

--- Silver/1018.py
+++ Silver/1018.py
@@ -2,8 +2,9 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 Board_list = [0]*N
-Operator_list = []
+Operator_list = [0]*8
 result = 0
+result_list = []
 
 for i in range(0, len(Board_list)):
   column_list = [0]*M
@@ -17,14 +18,43 @@ for i in range(0, len(Board_list)):
   Board_list[i] = column_list
   
 
-for r in range(N-8):
-  for c in range(M-8):
-    
-    for row in range(r, r+9):
+for r in range(N-7):
+  for c in range(M-7):
+    row_count = -1
+    for row in range(r, r+8):
+      row_count += 1
       column_list = [0]*8
-      for col in range(c, c+9):
-        column_list[col] = Board_list[row, col]
-    print(column_list)
+      col_count = -1
+      for col in range(c, c+8):
+        col_count += 1
+        column_list[col_count] = Board_list[row][col]
+      Operator_list[row_count] = column_list
+    print(Operator_list)
+    
+    result = 0
+    for R in range(0, 8):
+      for C in range(0, 7):
+        if (R != 0 and C == 0):
+          if Operator_list[R-1][C] + Operator_list[R][C] == 0:
+            result += 1
+            if Operator_list[R][C] == 1:
+              Operator_list[R][C] = 0
+            else:
+              Operator_list[R][C] = 1
+        if Operator_list[R][C] + Operator_list[R][C+1] == 1:
+          continue
+        else:
+          result += 1
+          if Operator_list[R][C+1] == 1:
+            Operator_list[R][C+1] = 0
+          else:
+            Operator_list[R][C+1] = 1
+    result_list.append(result)
+    print(result_list[-1])
 
 
 print(Board_list)
+print(Operator_list)
+result_list.sort()
+print(result_list)
+print(result_list[0], len(result_list))
