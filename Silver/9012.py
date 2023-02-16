046ab4e2f4962d8805e8051ce430c3051f302d8b
AI-WonYJ <yjason7587@naver.com>
2023-02-17 00:51:53 +0900

    Silver 4 9012번; 괄호

--- Silver/9012.py
+++ Silver/9012.py
@@ -4,16 +4,18 @@ from collections import deque
 N = int(sys.stdin.readline())
 
 for i in range(N):
-  S = sys.stdin.readline()
-  l = ''
+  S = sys.stdin.readline().rstrip()
+  result = ''
   N_deque = deque()
   for j in range(0, len(S)):
     if S[j] == '(':
       N_deque.insert(j,'(')
       N_deque.insert(j+1,')')
+
   for k in N_deque:
-    l += k
-  if S[:-1] == l:
+    result += k
+
+  if S == result:
     print('YES')
   else:
     print('NO')
