1b95424724d1e6f1216718c5428bf1e6d873e8d0
AI-WonYJ <yjason7587@naver.com>
2023-02-17 02:11:39 +0900

    Silver 4 10828번: 스택

---
+++ Silver/10828.py
@@ -0,0 +1,45 @@
+from collections import deque
+import sys
+
+N_deque = deque()
+
+def push(x):
+  N_deque.appendleft(x)
+
+def pop():
+  if len(N_deque) == 0:
+    print(-1)
+  else:
+    print(N_deque.popleft())
+
+def size():
+  print(len(N_deque))
+
+def empty():
+  if len(N_deque) == 0:
+    print(1)
+  else:
+    print(0)
+
+def top():
+  if len(N_deque) == 0:
+    print(-1)
+  else:
+    print(N_deque[0])
+
+
+N = int(sys.stdin.readline().rstrip())
+c_deque = deque()
+
+for i in range(N):
+  c = deque(map(str, sys.stdin.readline().rstrip().split()))
+  if c[0] == 'push':
+    push(int(c[1]))
+  elif c[0] == 'pop':
+    pop()
+  elif c[0] == 'size':
+    size()
+  elif c[0] == 'empty':
+    empty()
+  elif c[0] == 'top':
+    top()
