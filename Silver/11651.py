efe42d75a827ef6dcd7df3e75a9a8865c115b4ee
AI-WonYJ <yjason7587@naver.com>
2023-02-19 23:50:06 +0900

    Silver 5 11651번: 좌표 정렬하기 2

--- Silver/11651.py
+++ Silver/11651.py
@@ -1,3 +1,21 @@
+# 정답 코드 1
+import sys
+
+N = int(sys.stdin.readline())
+N_list = list(range(0, N))
+
+for i in range(N):
+  x, y = map(int, sys.stdin.readline().split())
+  N_list[i] = [x, y]
+
+N_list.sort(key=lambda y: (y[1], y[0]))
+
+for n in N_list:
+  print(n[0], n[1])
+
+
+
+# 정답 코드 2
 import sys
 
 N = int(sys.stdin.readline())
@@ -8,20 +26,20 @@ for i in range(N):
   N_list[i] = [x, y]
 
 N_list.sort(key=lambda y: y[1])
-N_sort_list = []
 
 j = 0
 while (j < (len(N_list)-1)):
+  N_sort_list = []
   count = 1
   if N_list[j][1] == N_list[j+1][1]:
-    N_sort_list.append(N_list[j])
-    k = (j + 1)
-    while N_list[k-1][1] == N_list[k][1]:
+    k = j
+    while N_list[k][1] == N_list[k + 1][1]:
       N_sort_list.append(N_list[k])
       k += 1
       count += 1
       if (k+1) >= len(N_list):
         break
+    N_sort_list.append(N_list[k])
     N_sort_list.sort(key=lambda x: x[0])
     for l in range(0, len(N_sort_list)):
       N_list[l + j] = N_sort_list[l]
