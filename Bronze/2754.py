d2321ca44a9be71603ad531bf15bb3808480022f
AI-WonYJ <yjason7587@naver.com>
2023-02-25 15:37:08 +0900

    Bronze 5 2754번: 학점계산

--- Bronze/2754.py
+++ Bronze/2754.py
@@ -1,6 +1,6 @@
 import sys
 
-G_list = [['A', 4.0], ['B', 3.0], ['C', 2.0], ['D', 1.0], [0], ['F', 0.0]]
+G_list = [['A', 4.0], ['B', 3.0], ['C', 2.0], ['D', 1.0], [0, 0], ['F', 0.0]]
 grade = str(sys.stdin.readline().rstrip())
 
 point = G_list[ord(grade[0])-65][1]
