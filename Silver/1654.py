763b0e0132c61d8da8deaba54450b99a69dba1ea
AI-WonYJ <yjason7587@naver.com>
2023-03-01 23:11:01 +0900

    Silver 2 1654번: 랜선 자르기

--- Silver/1654.py
+++ Silver/1654.py
@@ -1,11 +1,9 @@
 import sys
 
-def Cut(li, n, ma):
+def Cut(li, n):
   count = 0
   for j in range(0, len(li)):
     count += (li[j] // n)
-    if count > ma:
-      break
   return count
 
 N, K = map(int, sys.stdin.readline().rstrip().split())
@@ -18,27 +16,17 @@ N_list.sort()
 
 start = 0
 end = (2**31)-1
-repe = 0
-while True:
-  if N == 1 and K == 1:
-    print(N)
-    break
-  mid = (start + end) // 2
-  if mid == repe:
-    print(mid)
-    break
-  value = Cut(N_list, mid, K)
-  if value == K:
-    for k in range(mid, end):
-      value = Cut(N_list, k, K)
-      if value < K:
-        print(k - 1)
-        break
-    break
-  elif value > K:
-    start = mid + 1
-    repe = mid
-  elif value < K:
-    end = mid -1
-    repe = mid
-
+max = 0
+if N == 1 and K == 1:
+  print(N_list[0])
+else:
+  while start <= end:
+    mid = (start + end) // 2
+    value = Cut(N_list, mid)
+    if value >= K:
+      max = mid
+      start = mid + 1
+    elif value < K:
+      end = mid - 1
+  
+  print(max)
