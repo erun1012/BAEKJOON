6fefc7fe7ff11d0be16fbe42113851240233e224
AI-WonYJ <yjason7587@naver.com>
2023-03-02 23:40:45 +0900

    Silver 2 2805번: 나무 자르기

---
+++ Silver/2805.py
@@ -0,0 +1,30 @@
+import sys
+
+def Sum(LIST, Leng):
+  sum = 0
+  for i in range(0, len(LIST)):
+    if LIST[i] > Leng:
+      sum += (LIST[i] - Leng)
+  return sum
+
+N, M = map(int, sys.stdin.readline().rstrip().split())
+
+N_list = list(map(int, sys.stdin.readline().rstrip().split()))
+
+N_list.sort()
+
+start, end = 0, N_list[-1]
+max = 0
+
+if N == 1:
+  print(N_list[0] - M)
+else:
+  while start <= end:
+    mid = (start + end) // 2
+    value = Sum(N_list, mid)
+    if value >= M:
+      max = mid
+      start = mid + 1
+    elif value < M:
+      end = mid - 1
+  print(max)
