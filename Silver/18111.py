8e75c7ab85a761f29bc799284ecd732f6c76883f
AI-WonYJ <yjason7587@naver.com>
2023-03-03 00:19:22 +0900

    Silver 2 2805번: 나무 자르기 못풀겠음

--- Silver/18111.py
+++ Silver/18111.py
@@ -1,3 +1,13 @@
 import sys
 
 N, M, B = map(int, sys.stdin.readline().rstrip().split())
+N_list = [0] * N
+
+for j in range(0, len(N_list)):
+  N_list[j] = list(map(int, sys.stdin.readline().rstrip().split()))
+
+measure = 0
+high, time1, time2 = 0, 0, 0
+for k in range(0, len(N_list)):
+  for l in range(0, len(N_list[k])):
+    if N_list[k][l] >= N_list[0]
