00e9c297d2faecd686156834c4b98ee619c756b2
AI-WonYJ <yjason7587@naver.com>
2023-03-14 16:02:42 +0900

    Change

--- 0_PS/1_Week/F.py
+++ 0_PS/1_Week/F.py
@@ -12,19 +12,28 @@ for k in range(0, len(N_list)):
   if N_list[k] != 0:
     count_list.append([k+1, N_list[k]])
 
+count_list.sort(key=lambda x : x[1])
+count_list.reverse()
+
 result = [0]*12
 for j in range(0, len(A)):
   if A[j] == "Y":
-    if j <= 5:
+    if j <= 5:  # solved
       result[j] = (N_list[j]+2)*(j+1)
-    elif j == 6:
-      if int(Counter(N_list).most_common()[0][1]) >= 2:
-        result[j] = (int(Counter(N_list).most_common()[0][0]))*(int(Counter(N_list).most_common()[0][1])+2)
+    elif j == 6: # solved
+      if count_list[0][1] >= 2:
+        result[j] = count_list[0][0] * 4
     elif j == 7:
       if len(count_list) == 1:
-        result[j] = count_list[0][0]*3 + 6*2
+        if count_list[0][0] == 6:
+          result[j] = count_list[0][0]*3 + 5*2
+        else:
+          result[j] = count_list[0][0]*3 + 6*2
       elif len(count_list) == 2:
-        result[j] = count_list[0][0]*count_list[0][1] + count_list[1][0]*(count_list[1][1]+2)
+        if count_list[0][1] == 1:
+          result[j] = count_list[0][0]*(count_list[0][1]+1) + count_list[1][0]*(count_list[1][1]+1)
+        elif count_list[0][1] == 2:
+          result[j] = count_list[0][0]*count_list[0][1] + count_list[1][0]*(count_list[1][1]+2)
     elif j == 8:
       if N_list[5] == 0 and len(count_list) == 3:
         result[j] = 30
@@ -38,6 +47,6 @@ for j in range(0, len(A)):
       sum = 0
       for k in range(0, len(count_list)):
         sum += count_list[k][0]*count_list[k][1]
-      result[j] = sum
+      result[j] = sum + 6*2
 
 print(max(result))
