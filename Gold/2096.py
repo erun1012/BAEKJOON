4e9e8ebbf2f5d81e94e05690e9576c3e84a6d56d
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2024-03-10 14:18:35 +0900

    Gold 5 2096번: 내려가기

---
+++ Gold/2096.py
@@ -0,0 +1,22 @@
+from sys import stdin
+import copy
+
+N = int(stdin.readline())
+
+arr = []
+
+for i in range(N):
+    A, B, C = map(int, stdin.readline().rstrip().split())
+    arr.append([[A, A], [B, B], [C, C]])
+    
+    if len(arr) == 2:
+        arr[1][0][0] += max(arr[0][0][0], arr[0][1][0])
+        arr[1][1][0] += max(arr[0][0][0], arr[0][1][0], arr[0][2][0])
+        arr[1][2][0] += max(arr[0][1][0], arr[0][2][0])
+        arr[1][0][1] += min(arr[0][0][1], arr[0][1][1])
+        arr[1][1][1] += min(arr[0][0][1], arr[0][1][1], arr[0][2][1])
+        arr[1][2][1] += min(arr[0][1][1], arr[0][2][1])
+        
+        arr.pop(0)
+
+print(max(arr[0][0][0], arr[0][1][0], arr[0][2][0]), min(arr[0][0][1], arr[0][1][1], arr[0][2][1]))
