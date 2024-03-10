ba8d85dac287c358bd6fba5380dcabf7ec43f294
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2024-03-10 11:25:25 +0900

    Silver 1 1149번: RGB거리

---
+++ Silver/1149.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+N = int(stdin.readline())
+
+arr = [list(map(int, stdin.readline().rstrip().split())) for _ in range(N)]
+
+for i in range(1, N):
+    arr[i][0] += min(arr[i-1][1], arr[i-1][2])
+    arr[i][1] += min(arr[i-1][0], arr[i-1][2])
+    arr[i][2] += min(arr[i-1][0], arr[i-1][1])
+
+print(arr)
+print(min(arr[N-1][0], arr[N-1][1], arr[N-1][2]))
