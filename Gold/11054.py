b897b6746757a7ea8ce1ce58906ca29cde325555
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2024-03-11 14:29:49 +0900

    Gold 4 11054번: 가장 긴 바이토닉 부분 수열

--- Gold/11054.py
+++ Gold/11054.py
@@ -2,23 +2,22 @@ from sys import stdin
 
 N = int(stdin.readline())
 get = list(map(int, stdin.readline().rstrip().split()))
+reverse_get = get[::-1]
 
-arr = [1]*N
+increase_arr = [1 for _ in range(N)]
+decrease_arr = [1 for _ in range(N)]
 
-for i in range(0, N):
-    for j in range(0, i):
+for i in range(N):
+    for j in range(i):
         if get[i] > get[j]:
-            arr[i] = max(arr[i], arr[j]+1)
+            increase_arr[i] = max(increase_arr[i], increase_arr[j]+1)
+        if reverse_get[i] > reverse_get[j]:
+            decrease_arr[i] = max(decrease_arr[i], decrease_arr[j]+1)
 
-if arr.index(max(arr))+1 == N:
-    print(max(arr))
-else:
-    check = arr[arr.index(max(arr))+1:]
-    check.reverse()
-    check_arr = [1]*len(check)
-    
-    for k in range(0, len(check)):
-        for l in range(0, k):
-            if check[k] > check[l]:
-                check_arr[k] = max(check_arr[k], check_arr[l]+1)
-    print(max(arr)+max(check_arr))
+
+result = [0 for _ in range(N)]
+decrease_arr.reverse()
+
+for i in range(N):
+    result[i] = increase_arr[i] + decrease_arr[i]-1
+print(max(result))
