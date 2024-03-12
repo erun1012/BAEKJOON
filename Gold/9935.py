000083875b7a39395f3c3913d7a60d68d2a0ad25
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2024-03-12 18:01:45 +0900

    Gold 4 9935번: 문자열 폭발

--- Gold/9935.py
+++ Gold/9935.py
@@ -3,9 +3,16 @@ from sys import stdin
 get = stdin.readline().rstrip()
 bomb = stdin.readline().rstrip()
 
-while get.find(bomb) != -1:
-    get = get.replace(bomb,"")
-if len(get) > 0:
-    print(get)
-else:
+lastbomb = bomb[-1]
+list = []
+
+for char in get:
+    list.append(char)
+    if char == lastbomb and ''.join(list[-len(bomb):]) == bomb:
+        del list[-len(bomb):]
+        
+result = ''.join(list)
+if result == '':
     print("FRULA")
+else:
+    print(result)
