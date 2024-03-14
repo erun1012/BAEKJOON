768aba6113a06e49ff740208f40f4c75cbf065de
YoungJin <101448204+AI-WonYJ@users.noreply.github.com>
2024-03-14 19:13:50 +0900

    Silver 2 1260번: DFS와 BFS

---
+++ Silver/1260.py
@@ -0,0 +1,38 @@
+from sys import stdin
+
+def bfs(graph, start_node):
+    need_visited, visited = list(), list()
+    need_visited.append(start_node)
+    while need_visited:
+        node = need_visited.pop(0)
+        if node not in visited:
+            visited.append(node)
+            need_visited.extend(graph[node])
+    return visited
+    
+def dfs(graph, start_node):
+    need_visited, visited = list(), list()
+    need_visited.append(start_node)
+    while need_visited:
+        node = need_visited.pop(0)
+        if node not in visited:
+            visited.append(node)
+            for i in range(len(graph[node])-1, -1, -1):
+                need_visited.insert(0, graph[node][i])
+    return visited
+    
+N, M, V = map(int, stdin.readline().rstrip().split())
+
+graph = dict()
+for i in range(1, N+1):
+    graph[i] = []
+
+for i in range(M):
+    A, B = map(int, stdin.readline().rstrip().split())
+    graph[A].append(B)
+    graph[B].append(A)
+
+for i in range(1, N+1):
+    graph[i].sort()
+print(dfs(graph, V))
+print(bfs(graph, V))
