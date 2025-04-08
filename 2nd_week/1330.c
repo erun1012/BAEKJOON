0409185381796cc35b68ad5193d8c3c9da2122d2
erun1012 <128389168+erun1012@users.noreply.github.com>
2025-04-08 14:11:59 +0900

    Update 1330.c

--- 2nd_week/1330.c
+++ 2nd_week/1330.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+int main() {
+int a;
+int b;
+
+scanf("%d %d",&a,&b);
+
+
+if (a>b) {
+    printf(">");
+}else if (a<b) {
+    printf("<");
+} else {
+    printf("==");
+    
+}
+return 0;
+}
